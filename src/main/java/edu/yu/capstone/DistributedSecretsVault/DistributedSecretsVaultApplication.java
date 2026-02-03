4c0518e8d792c6ec96df606279f76ed131b28590
Max F <meirmaxf11@gmail.com>
2026-02-03 17:14:43 -0500

    initialize spring boot project

---
+++ src/main/java/edu/yu/capstone/DistributedSecretsVault/DistributedSecretsVaultApplication.java
@@ -0,0 +1,13 @@
+package edu.yu.capstone.DistributedSecretsVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSecretsVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedSecretsVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/yu/capstone/DistributedSecretsVault/DistributedSecretsVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yu.capstone.DistributedSecretsVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSecretsVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
