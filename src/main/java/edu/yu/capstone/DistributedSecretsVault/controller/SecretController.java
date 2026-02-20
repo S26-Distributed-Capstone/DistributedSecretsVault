cf44d7795e4a9fea8addde9c0358b57e3b854ee9
Noam Ben Simon <noampvb@gmail.com>
2026-02-19 22:25:50 -0500

    validly added Put and Delete, along with necessary skeletons for arguments.

--- src/main/java/edu/yu/capstone/DistributedSecretsVault/controller/SecretController.java
+++ src/main/java/edu/yu/capstone/DistributedSecretsVault/controller/SecretController.java
@@ -3,13 +3,17 @@ package edu.yu.capstone.DistributedSecretsVault.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.yu.capstone.DistributedSecretsVault.dto.secret.DeleteSecretRequest;
 import edu.yu.capstone.DistributedSecretsVault.dto.secret.PostSecretRequest;
+import edu.yu.capstone.DistributedSecretsVault.dto.secret.PutSecretRequest;
 import edu.yu.capstone.DistributedSecretsVault.service.secret.*;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -18,10 +22,15 @@ public class SecretController {
 
     private final GetSecretService getSecretService;
     private final PostSecretService postSecretService;
+    private final PutSecretService putSecretService;
+    private final DeleteSecretService deleteSecretService;
 
-    public SecretController(GetSecretService getSecretService, PostSecretService postSecretService) {
+    public SecretController(GetSecretService getSecretService, PostSecretService postSecretService,
+            PutSecretService putSecretService, DeleteSecretService deleteSecretService) {
         this.getSecretService = getSecretService;
         this.postSecretService = postSecretService;
+        this.putSecretService = putSecretService;
+        this.deleteSecretService = deleteSecretService;
     }
 
     @GetMapping("/{id}")
@@ -32,6 +41,16 @@ public class SecretController {
     @PostMapping
     public ResponseEntity<String> postSecret(@RequestBody PostSecretRequest request) {
         return this.postSecretService.execute(request);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateSecret(@RequestBody PutSecretRequest request) {
+        return this.putSecretService.execute(request);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteSecret(@RequestBody DeleteSecretRequest request) {
+        return this.deleteSecretService.execute(request);
     }
 
 }
