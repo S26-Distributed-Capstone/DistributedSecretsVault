dbf15ef0ac132da5576472135ad2bbd31a1be44f
Ari Krakauer <krakauer@mail.yu.edu>
2026-02-22 20:08:42 -0500

    Added missing annotations

--- src/test/java/edu/yu/capstone/DistributedSecretsVault/controller/SecretControllerTest.java
+++ src/test/java/edu/yu/capstone/DistributedSecretsVault/controller/SecretControllerTest.java
@@ -1,11 +1,11 @@
 package edu.yu.capstone.DistributedSecretsVault.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-/*
- * Use @WebMvcTest to test only the web layer
- * Mock the service layer with @MockBean
- */
+@WebMvcTest
+@MockitoBean
 public class SecretControllerTest {
     @Test
     public void testCreateSecretHappyPath() {
