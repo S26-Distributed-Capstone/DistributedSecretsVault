8e0e80d7e34da95c276781ab56e6a2457661a722
Ari Krakauer <krakauer@mail.yu.edu>
2026-02-22 20:12:17 -0500

    Commented out MockitoBean annotation which caused error

--- src/test/java/edu/yu/capstone/DistributedSecretsVault/controller/SecretControllerTest.java
+++ src/test/java/edu/yu/capstone/DistributedSecretsVault/controller/SecretControllerTest.java
@@ -5,7 +5,7 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @WebMvcTest
-@MockitoBean
+//@MockitoBean() - fill in blank with mock implementation (I think that's what's causing the problem)
 public class SecretControllerTest {
     @Test
     public void testCreateSecretHappyPath() {
