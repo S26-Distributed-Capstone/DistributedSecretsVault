934170b719064d54d021fb97697127342311ccae
Noam Ben Simon <noampvb@gmail.com>
2026-02-24 17:28:42 -0500

    fixed bean implementation missing

--- src/test/java/edu/yu/capstone/DistributedSecretsVault/controller/SecretControllerTest.java
+++ src/test/java/edu/yu/capstone/DistributedSecretsVault/controller/SecretControllerTest.java
@@ -3,10 +3,23 @@ package edu.yu.capstone.DistributedSecretsVault.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import edu.yu.capstone.DistributedSecretsVault.service.secret.*;
 
-@WebMvcTest
-//@MockitoBean() - fill in blank with mock implementation (I think that's what's causing the problem)
+@WebMvcTest(SecretController.class)
 public class SecretControllerTest {
+    
+    @MockitoBean
+    private GetSecretService getSecretService;
+    
+    @MockitoBean
+    private PostSecretService postSecretService;
+    
+    @MockitoBean
+    private PutSecretService putSecretService;
+    
+    @MockitoBean
+    private DeleteSecretService deleteSecretService;
+    
     @Test
     public void testCreateSecretHappyPath() {
 
