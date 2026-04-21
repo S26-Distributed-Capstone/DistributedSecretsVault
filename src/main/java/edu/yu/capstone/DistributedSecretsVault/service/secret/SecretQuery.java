6e3ab1c16d922fe21c9ba7b834f416b6dc2f9a6c
Noam Ben Simon <noampvb@gmail.com>
2026-04-21 18:01:12 -0400

    removed now-unused query interface

--- src/main/java/edu/yu/capstone/DistributedSecretsVault/service/secret/SecretQuery.java
+++
@@ -1,9 +0,0 @@
-package edu.yu.capstone.DistributedSecretsVault.service.secret;
-
-import org.springframework.http.ResponseEntity;
-
-public interface SecretQuery<I, O> {
-
-    ResponseEntity<O> execute(I input);
-
-}
